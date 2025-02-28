99d1d5e1fdb25b449cedc6380827f97477809126
Moritz <mosum@google.com>
2025-02-28 12:54:38 +0100

    Fixes

--- pkgs/assets/hook/dsad.dart
+++
@@ -1,7 +0,0 @@
-import 'package:assets/src/assets.dart';
-
-Future<void> main(List<String> arguments) async {
-  final asset = const StringAsset('package/myPackageName/myData1');
-  final json = await asset.load();
-  print(json);
-}
