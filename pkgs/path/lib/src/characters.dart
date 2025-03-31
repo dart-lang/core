a6e81e0b74faf12ed4a938fa9a8ddba86b89b73d
Lasse R.H. Nielsen <lrn@google.com>
2025-03-31 13:29:24 +0200

    Make URL strategy better at recognizing URLs. (#873)

--- pkgs/path/lib/src/characters.dart
+++ pkgs/path/lib/src/characters.dart
@@ -2,7 +2,10 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// This library contains character-code definitions.
+// Character-code constants.
+
+const hash = 0x23;
+const percent = 0x25;
 const plus = 0x2b;
 const minus = 0x2d;
 const period = 0x2e;
@@ -10,8 +13,13 @@ const slash = 0x2f;
 const zero = 0x30;
 const nine = 0x39;
 const colon = 0x3a;
+const question = 0x3f;
 const upperA = 0x41;
 const upperZ = 0x5a;
 const lowerA = 0x61;
+const lowerE = 0x65;
+const lowerF = 0x66;
+const lowerI = 0x69;
+const lowerL = 0x6c;
 const lowerZ = 0x7a;
 const backslash = 0x5c;
