1030c1d8e27cf83e026d05875a59dfb577ef0a9c
Lasse R.H. Nielsen <lrn@google.com>
2025-04-07 13:56:24 +0200

    New API with per-platform properties.
    The `Platform.current` object exposes one (or potentially more)
    of a `NativePlatform` (based on `dart:io`) or `BrowserPlatform`
    (based on JS `window.navigator` or anything else relevant).
    
    Each has its own API, and the current platform determines
    whether a value of these types is available or not.
    On unsupported platforms, a value is just `null`.
    
    Provides a `testing.dart` library which exposes
    `FakeNativePlatform` and `FakeBrowserPlatform`,
    which are customizable classes that can be used for testing.
    It's possible to run code with such a fake value as
    the *current* platform (`Platform.current` is temporarily
    updated to a value which exposes the, fx, `FakeNativePlatform`
    as its `Platform.nativePlatform`).
    
    Still retains the legacy and deprecated API that makes
    native properties directly available on `Platform`.
    Code should migrate to the new API by doing:
    * `const LocalPlatform()` &rarr; `Platform.current.nativePlatform!`
    * `new FakePlatform()` &rarr; `new FakeNativePlatform()`
    
    It's currently not possible to have more than one
    non-`null` platform-value.
    Platforms are detected by the presence of the `dart:io` and `dart:js_interop`
    libraries, and no current platform has both.
    When such a platform exists, the code will need to be adapted to to produce
    *both* (possible using conditional imports or conditional parts
    and augmentations).

---
+++ pkgs/platform/test/browser_platform_test.dart
@@ -0,0 +1,68 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// Test of [browserPlatform].
+@OnPlatform({'!browser': Skip('Browser-only functionality')})
+library;
+
+import 'dart:js_interop'; // For `appVersion` and `navigator`.
+
+import 'package:platform/platform.dart';
+import 'package:test/test.dart';
+
+void main() {
+  const isNative = bool.fromEnvironment('dart.library.io');
+  const isBrowser = bool.fromEnvironment('dart.library.js_interop');
+  final platform = Platform.current;
+  final browserPlatform = platform.browserPlatform;
+
+  test('Running on browser', () {
+    expect(isBrowser, true, reason: 'dart.library.js_interop');
+    expect(isNative, false, reason: 'dart.library.io');
+    expect(platform.isNative, false, reason: 'Platform.current.isNative');
+    expect(platform.isBrowser, true, reason: 'Platform.current.isBrowser');
+    expect(
+      browserPlatform,
+      isNotNull,
+      reason: 'Platform.current.browserPlatform',
+    );
+    expect(
+      platform.nativePlatform,
+      isNull,
+      reason: 'Platform.current.nativePlatform',
+    );
+  });
+
+  if (browserPlatform == null) return; // Promote to non-null.
+
+  test('static properties', () {
+    // The `current` getter is the same objects as
+    // `Platform.current.browserPlatform`.
+    expect(
+      browserPlatform,
+      same(BrowserPlatform.current),
+      reason: 'BrowserPlatform.current',
+    );
+  });
+
+  test('Has same values as JS', () {
+    // Everything is taken from browser (`window.navigator`).
+    expect(
+      browserPlatform.userAgent,
+      userAgent,
+      reason: 'window.navigator.userAgent',
+    );
+    expect(
+      browserPlatform.version,
+      appVersion,
+      reason: 'window.navigator.appVersion',
+    );
+  });
+}
+
+@JS('window.navigator.appVersion')
+external String get appVersion;
+
+@JS('window.navigator.userAgent')
+external String get userAgent;
