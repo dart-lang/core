2aa4b6ef30fe4c09cdfdd52f79169c03ce3f402f
Lasse R.H. Nielsen <lrn@google.com>
2025-04-07 13:56:24 +0200

    New API with per-platform properties.
    The `Platform.current` object exposes one (or potentially more)
    of a `NativePlatform` (based on `dart:io`) or `BrowserPlatform`
    (based on JS `window.navigator` or anything else relevant).
    
    Each has its own API, and the current platform determines
    whether a value of these types is available or not.
    On unsupported platforms, a value is just `null`.
    
    Provides a `testing.dart` library which exposes
    `FakeNativePlatform` and `FakeBrowserPlatform`,
    which are customizable classes that can be used for testing.
    It's possible to run code with such a fake value as
    the *current* platform (`Platform.current` is temporarily
    updated to a value which exposes the, fx, `FakeNativePlatform`
    as its `Platform.nativePlatform`).
    
    Still retains the legacy and deprecated API that makes
    native properties directly available on `Platform`.
    Code should migrate to the new API by doing:
    * `const LocalPlatform()` &rarr; `Platform.current.nativePlatform!`
    * `new FakePlatform()` &rarr; `new FakeNativePlatform()`
    
    It's currently not possible to have more than one
    non-`null` platform-value.
    Platforms are detected by the presence of the `dart:io` and `dart:js_interop`
    libraries, and no current platform has both.
    When such a platform exists, the code will need to be adapted to to produce
    *both* (possible using conditional imports or conditional parts
    and augmentations).

---
+++ pkgs/platform/tool/src/sample/legacy_example_code_2.dart
@@ -0,0 +1,67 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// Example code using legacy APIs. Used to test automatic migration.
+// Uses every legacy API member.
+//
+// To test migration of this file, run `dart tool/check_migration.dart` from
+// the project root.
+
+// ignore_for_file: omit_local_variable_types, prefer_const_constructors
+
+// REMOVE NEXT LINE BEFORE TESTING MIGRATION DIRECTLY:
+// ignore_for_file: deprecated_member_use_from_same_package
+// (The `tools/test_migration.dart` script removes the line.)
+
+import 'package:platform/platform.dart';
+import 'package:platform/platform.dart' as prefix;
+
+void main() {
+  direct();
+  withPrefix();
+}
+
+void direct() {
+  LocalPlatform local = LocalPlatform();
+  FakePlatform fakePlatform = FakePlatform.fromJson(
+    local.toJson(),
+  ).copyWith(executable: 'pineapple');
+
+  // Use as type.
+  if ((local as dynamic) is! Platform) {
+    print('Local platform is not platform?');
+  }
+  if ((local as dynamic) is! LocalPlatform) {
+    print('Local platform is not local platform?');
+  }
+  if ((fakePlatform as dynamic) is! FakePlatform) {
+    print('Fake platform is not fake platform?');
+  }
+
+  Platform platform = fakePlatform;
+  if (fakePlatform.executable == 'apricot') throw AssertionError('Not it');
+  if (platform.executable == 'apricot') throw AssertionError('Not it');
+}
+
+void withPrefix() {
+  prefix.LocalPlatform local = prefix.LocalPlatform();
+  prefix.FakePlatform fakePlatform = prefix.FakePlatform.fromJson(
+    local.toJson(),
+  ).copyWith(executable: 'pineapple');
+
+  // Use as type.
+  if ((local as dynamic) is! prefix.Platform) {
+    print('Local platform is not platform?');
+  }
+  if ((local as dynamic) is! prefix.LocalPlatform) {
+    print('Local platform is not local platform?');
+  }
+  if ((fakePlatform as dynamic) is! prefix.FakePlatform) {
+    print('Fake platform is not fake platform?');
+  }
+
+  prefix.Platform platform = fakePlatform;
+  if (fakePlatform.executable == 'apricot') throw AssertionError('Not it');
+  if (platform.executable == 'apricot') throw AssertionError('Not it');
+}
