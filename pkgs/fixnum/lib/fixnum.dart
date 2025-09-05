8bfaa9dc5394ab85dc8ccb6d368796dd7aa83389
Ömer Ağacan <omersa@google.com>
2025-09-05 12:13:26 +0100

    Comment formatting

--- pkgs/fixnum/lib/fixnum.dart
+++ pkgs/fixnum/lib/fixnum.dart
@@ -4,9 +4,8 @@
 
 /// Signed 32- and 64-bit integer support.
 ///
-/// The integer implementations in this library are designed to work
-/// identically whether executed on the Dart VM or compiled to JavaScript or
-/// Wasm.
+/// The integer implementations in this library are designed to work identically
+/// whether executed on the Dart VM or compiled to JavaScript or Wasm.
 library fixnum;
 
 export 'src/int32.dart';
