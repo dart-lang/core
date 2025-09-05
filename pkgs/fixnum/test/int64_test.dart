c481b839417e4f296b7b132658f28ff3f6484bad
Ömer Ağacan <omersa@google.com>
2025-09-05 12:15:42 +0100

    Suppress test analysis errors

--- pkgs/fixnum/test/int64_test.dart
+++ pkgs/fixnum/test/int64_test.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// ignore_for_file: prefer_const_constructors
+
 // We permit local variable types in this test because they statically 'assert'
 // that the operations have an expected type.
 //
